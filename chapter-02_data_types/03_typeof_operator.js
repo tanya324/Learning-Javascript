dc35037321dcd93212dc2d2def27a1ce78342175
Tanya Srivastava <tanyashrivastava2020@gmail.com>
2026-03-24 23:37:13 +0530

    typeof operator

---
+++ chapter-02_data_types/03_typeof_operator.js
@@ -0,0 +1,14 @@
+// typoeof Operator
+
+console.log(typeof "Hello");
+console.log(typeof 42);     
+console.log(typeof true);
+console.log(typeof {});      
+console.log(typeof undefined);
+
+// Output:
+// string
+// number
+// boolean
+// object
+// undefined
