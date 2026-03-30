9f11d1251ae5326dc0e04aec33e5b3d91d49ccf8
Tanya Srivastava <tanyashrivastava2020@gmail.com>
2026-03-30 14:17:01 +0530

    arthimetic operators

---
+++ chapter-03_operators/01_arithmetic_operators.js
@@ -0,0 +1,7 @@
+const sum = 5 + 3; // 8
+const diff = 10 - 2; // 8 
+const mul = 4 * 2; //  8
+const div = 8 / 2; // 4
+const rem = 10 % 3; // 1
+const exp = 2 ** 3; // 8
+console.log(sum, diff, mul, div, rem, exp);
