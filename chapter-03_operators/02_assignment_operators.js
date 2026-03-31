2fd8b1e6d19f8c09ec9731ededbe01d8d3e67b1b
Tanya Srivastava <tanyashrivastava2020@gmail.com>
2026-03-31 12:28:47 +0530

    assignment operators

---
+++ chapter-03_operators/02_assignment_operators.js
@@ -0,0 +1,9 @@
+let a = 12; //assigns value 12 to variable a
+
+a += 5; //equivalent to a = a + 5, now a is 17
+a -= 3; //equivalent to a = a - 3, now a is 14
+a *= 2; //equivalent to a = a * 2, now a is 28
+a /= 4; //equivalent to a = a / 4, now a is 7
+a %= 3; //equivalent to a = a % 3, now a is 1
+a **= 2; //equivalent to a = a ** 2, now a is 1 (1 raised to the power of 2 is still 1)
+console.log(a); // Output: 1
